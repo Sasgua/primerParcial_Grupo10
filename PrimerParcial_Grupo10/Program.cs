16edaa180277164a18f1e31e58019a768a709dcb
Valentin De la Reta <valentindelareta23@gmail.com>
2020-09-16 22:59:30 -0300

    first commit

---
+++ PrimerParcial_Grupo10/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcial_Grupo10
+{
+    class Program
+    {
+       
+        
+        
+
+        static void Main(string[] args)
+        {
+            int opc, total, contador, descuento1, descuento2;
+
+            int UltiOpc;
+            
+            contador = 0;
+
+            do
+            {
+
+                Console.WriteLine("Camisas PRADBIT – Ventas minoristas y mayoristas");
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("MENÚ PRINCIPAL: \n1 - Añadir camisa al carro de compras \n2 - Eliminar camisa del carro de compras \n3 - Salir");
+                Console.WriteLine("Ingrese una opción del menú:");
+                opc = int.Parse(Console.ReadLine());
+
+                // esta es la opcion de salida por si el usuario desea cerrar el programa 
+                if (opc == 3)
+                {
+                    Console.WriteLine("¿Esta seguro que desea salir? 1.SI / 2.NO ");
+                    UltiOpc = int.Parse(Console.ReadLine());
+                    if (UltiOpc <= 1)
+                    {
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        opc = 4;
+                    }
+                   
+                }
+
+                switch (opc)
+                {
+                    case 1:
+
+                        contador++;
+                        
+                        break;
+                    case 2:
+
+                        contador--;
+
+                        break;
+                }
+
+                // aca estan las condiciones que tiene el programa segun las compras de las camisas  
+
+                total = contador * 1000;
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("- Cantidad de camisas en el carro de compras: < {0} > \n-Precio unitario: $1000", contador);
+                if (contador >= 3 && contador <= 5)
+                {
+                    descuento1 = (10 * total) / 100;
+                    Console.WriteLine("- Precio total sin descuento: ${0} \n-Tipo de descuento aplicado: %10 ", total);
+                    Console.WriteLine("-Precio final con descuento: $ " + descuento1);
+                    Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                }
+                else if (contador > 5)
+                {
+                    descuento2 = (20 * total) / 100;
+                    Console.WriteLine("- Precio total sin descuento: ${0} \n-Tipo de descuento aplicado: %20 ", total);
+                    Console.WriteLine("-Precio final con descuento: $ " + descuento2);
+                    Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("- Precio total sin descuento: ${0} \n-Tipo de descuento aplicado: %0 ", total);
+                    Console.WriteLine("-Precio final con descuento: $ " + total);
+                    Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                }
+                if (contador < 0)
+                {
+                    Console.WriteLine("Usted no puede tener un cantidad negativa de camisas");
+                    Console.ReadKey();
+                }
+
+            } while (opc != 3);
+
+
+
+            Console.ReadKey();
+        }
+
+      
+        
+    }
+}
---
+++ ReadMe.md.txt
@@ -0,0 +1,23 @@
+1)	Los errores de sintaxis son fáciles de corregir y se deben a que escribimos en el orden incorrecto las sentencias del programa. Estos errores se muestran cuando el programa se compila, y de esa forma se pueden buscar fácilmente para corregirlos. Los errores de lógica se deben a que hemos creado nuestro algoritmo en forma errónea y aunque todas las sentencias estén correctas, el programa no lleva a cabo lo que deseamos. Estos errores son más difíciles de corregir y la mejor forma de evitarlos es por medio de un análisis previo adecuado del problema a resolver.
+Algunas veces sucede que olvidamos colocar la cadena entre comillas o que solamente se ha colocado la comilla del inicio. Es importante no olvidar las cadenas siempre entre las dos comillas. Si no lo hacemos de esta forma, el compilador no puede encontrar correctamente la cadena o dónde finaliza. La sentencia nos marcará un error de compilación.
+
+
+2)	A continuación, podemos apreciar los operadores relacionales en C#. La forma como están escritos sus signos dentro de los paréntesis es la forma como debemos colocarlos en nuestro programa:
+
+-	Igualdad (==)
+-	No igual (¡=)
+-	Mayor que (>)
+-	Menor que (<)
+-	Mayor que igual (>=)
+-	Menor que igual (<=)
+
+
+3)	La refactorización de código tiene el fin de depurar o limpiar el código lo que lo hace más fácil de mantener y escalar. Este procedimiento no agrega ni quita funcionalidad al programa, sino que permite que el código se vuelva más entendible para otros programadores a la hora de realizar mantenimientos 
+
+Los pasos principales son:
+1-	Entender la lógica del código. 
+2-	Agrupar la lógica en bloques. 
+3-	Corroborar los cambios que no hayan sido alterados
+
+
+4)	Dentro de un ArrayList se puede guardar cualquier tipo de dato, por lo que podemos usar para enteros, cadenas, flotantes incluso para tipos definidos por nosotros mismos. ArrayList es una clase en C#, por lo que va a tener métodos o funciones que nos permitirán trabajar con los datos.
